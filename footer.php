4b2b63965a681fe6fc7ea551a421e8e02bc229bf
alejandro yanez del rio <alex_ydr@hotmail.com>
2024-10-30 10:47:00 -0400

    hauteur du footer corrigée

--- footer.php
+++ footer.php
@@ -1,37 +1,40 @@
 <input type="checkbox" name="checkboxFooter" id="checkFoot" />
-    <label for="checkFoot">
-      <footer>
-        <div class="conteneurSousTitre">
-          <span class="material-symbols-rounded"> close </span>
-          <h3>
-            <span class="material-symbols-outlined"> visibility </span> À PROPOS
-          </h3>
-        </div>
+<label for="checkFoot">
+  <footer>
+    <div class="conteneurSousTitre">
+      <span class="material-symbols-rounded"> close </span>
+      <h3>
+        <span class="material-symbols-outlined"> visibility </span> À PROPOS
+      </h3>
+    </div>
 
-        <div class="contenuFooter">
-          <div class="liens">
-            <p>3800 rue Sherbrooke Est</p>
-            <p>
-              <a href="https://www.cmaisonneuve.qc.ca/">Site web du Collège</a>
-            </p>
-          </div>
-          <div class="sociaux">
-            <i class="fa-brands fa-instagram"></i
-            ><i class="fa-brands fa-behance"></i
-            ><i class="fa-brands fa-linkedin"></i>
-          </div>
-          <div class="contact">
-            <p>page contact?</p>
-          </div>
-          <div class="equipe">
-            <p>
-              Site web réalisé Par Camilia El moustarih, Tony Ton, Lohan
-              Moutquin, Lenny Tran, Alejandro yanez
-            </p>
-          </div>
-        </div>
-      </footer>
+    <div class="contenuFooter">
+      <div class="contact">
+        <p>3800 rue Sherbrooke Est</p>
+        <p>
+          <a href="https://www.cmaisonneuve.qc.ca/">Site web du Collège</a>
+        </p>
+      </div>
+      <div class="sociaux">
+        <i class="fa-brands fa-instagram"></i><i class="fa-brands fa-behance"></i><i class="fa-brands fa-linkedin"></i>
+      </div>
 
-<?php wp_footer(); ?>
-</body>
-</html>
+      <div class="equipe">
+        <p>Site web réalisé Par
+          <a href="">Camilia El moustarih, </a>
+          <a href="">Tony Ton,</a>
+          <a href="">Lohan
+            Moutquin, </a>
+          <a href="">Lenny Tran, </a>
+          <a href="">Alejandro yanez Del rio</a>
+
+
+        </p>
+      </div>
+    </div>
+  </footer>
+
+  <?php wp_footer(); ?>
+  </body>
+
+  </html>
