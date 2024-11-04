5612743c88080ef85ced7ea15a1e283a8b833c36
El Moustarih <camilia.elmoustarih@gmail.com>
2024-11-04 14:59:00 -0500

    Continuation travail mise en page 404

--- 404.php
+++ 404.php
@@ -11,21 +11,17 @@
         <!-- Bouton pour retourner à la page d'accueil -->
         <span></span>
         <a href="#entete"> <a href="<?php //echo get_bloginfo('url'); ?>"> <?php //echo get_bloginfo('name'); ?></a> </a>
-
-        <!-- Récupérer le formulaire de recherche -->
-        <span></span>
-        <?php //get_search_form(); ?>  
-
-        
+           
         <!-- Ajouter le menu avec toutes les catégories -->
-        <div id="liensCatFooter">
+        <!-- <div id="liensCatFooter">
             <?php //wp_nav_menu(array("container" => "nav", "menu" => "principal")); ?>
-        </div>
+        </div> -->
 
     </section>
         
         <!-- Image de la librairie médias -->
-        <!-- <a href="<?php //echo get_bloginfo('url'); ?>"><img src="https://gftnth00.mywhc.ca/tim14/wp-content/uploads/2024/04/logoIntra.png"></a> -->
+        <!-- Image temporaire -->
+        <a href="<?php echo get_bloginfo('url'); ?>"><img src="https://gftnth00.mywhc.ca/tim14/wp-content/uploads/2024/10/EricD.png"></a> 
         
 </div>
 
