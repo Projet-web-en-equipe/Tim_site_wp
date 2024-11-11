3a95cd98a06c89aa828156812aca05ae4de77889
El Moustarih <e0958030@cmaisonneuve.qc.ca>
2024-11-11 15:00:06 -0500

    Avancement style pages profs et ajout image low poly des profs

--- front-page.php
+++ front-page.php
@@ -1,14 +1,14 @@
 <?php
 get_header();
 ?>
-<div id="ctnAnim">
+<!-- <div id="ctnAnim">
     <div id="ctnTitre">
         <h1 class="titreH2 anim1">Bienvenue</h1>
         <h1 class="titreH2 anim2">au</h1>
         <h1 class="titreH1 anim3">TIM</h1>
         <h3 class="titreH3 anim4">Maisonneuve</h3>
     </div> 
-</div>
+</div> -->
 <img src="https://gftnth00.mywhc.ca/tim14/wp-content/uploads/2024/10/EricG.png" alt="inshallah" id="perso" />
 <canvas width="900" height="900"></canvas>
 
