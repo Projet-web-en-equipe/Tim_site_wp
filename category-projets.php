c82644d0b8028bc81c3d63c7c2997cfc9c7240e1
root <e2159338@cmaisonneuve.qc.ca>
2024-12-04 09:10:33 -0500

    ajustement filtre

--- category-projets.php
+++ category-projets.php
@@ -11,12 +11,25 @@
         ?>
         <input type="checkbox" id="touch">
         <label for="touch">
-            <span id="selected-category">Toutes les projets</span>
+            <span id="selected-category">
+                <?php
+                // Récupérer la catégorie enfant sélectionnée dans l'URL
+                $selected_child_slug = isset($_GET['child_category']) ? sanitize_text_field($_GET['child_category']) : '';
+
+                // Si une sous-catégorie est sélectionnée, afficher son nom
+                if ($selected_child_slug) {
+                    $selected_category = get_category_by_slug($selected_child_slug);
+                    echo esc_html($selected_category ? $selected_category->name : 'Tous les projets');
+                } else {
+                    echo 'Tous les projets'; // Valeur par défaut
+                }
+                ?>
+            </span>
             <div class="fleche"></div>
         </label>
         <ul class="slide">
             <?php if ($current_category): ?>
-                <li><a href="#" data-category-id="<?php echo $current_category->term_id; ?>" data-category-slug="">Toutes les projets</a></li>
+                <li><a href="#" data-category-id="<?php echo $current_category->term_id; ?>" data-category-slug="">Tous les projets</a></li>
                 <?php
                 // Récupérer les projets
                 $child_categories_args = array(
