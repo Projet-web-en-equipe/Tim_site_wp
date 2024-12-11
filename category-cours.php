0c29bfe05eee5f24bb46862bebc025722b4885eb
root <e2159338@cmaisonneuve.qc.ca>
2024-12-11 09:13:43 -0500

    ajustement cours

--- category-cours.php
+++ category-cours.php
@@ -74,7 +74,7 @@
             <section id="carrousel">
                 <?php
                 // Initialisation des catégories à vérifier
-                $required_categories = array('cours - 3d', 'cours - design', 'cours - jeu', 'cours - vidéo', 'cours - web');
+                $required_categories = array('cours - 3d', 'cours - design', 'cours - jeu', 'cours - vidéo', 'cours - web', 'cours - autre');
                 $processed_courses = array();
 
                 while ($query->have_posts()) : $query->the_post();
@@ -111,6 +111,10 @@
                             <?php elseif ($current_course === 'cours - web') : ?>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                                     <path d="M432 64L208 64c-8.8 0-16 7.2-16 16l0 16-64 0 0-16c0-44.2 35.8-80 80-80L432 0c44.2 0 80 35.8 80 80l0 224c0 44.2-35.8 80-80 80l-16 0 0-64 16 0c8.8 0 16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zM0 192c0-35.3 28.7-64 64-64l256 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 192zm64 32c0 17.7 14.3 32 32 32l192 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L96 192c-17.7 0-32 14.3-32 32z" />
+                                </svg>
+                            <?php elseif ($current_course === 'cours - autre') : ?>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                                    <path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" />
                                 </svg>
                             <?php else : ?>
                                 <img src="https://gftnth00.mywhc.ca/tim14/wp-content/uploads/2024/10/placeholder.png" alt="placeholder">
