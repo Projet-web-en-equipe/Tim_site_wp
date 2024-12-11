0ecb3b4c50e68ec3a719ae190a94f9b791b878ae
root <e2159338@cmaisonneuve.qc.ca>
2024-12-10 23:29:45 -0500

    ajoute icone pour future

--- category-futur.php
+++ category-futur.php
@@ -17,10 +17,22 @@
             <section id="carrousel">
                 <?php
                 while ($query->have_posts()) : $query->the_post();
+
+                    // Définir l'URL de l'image selon le slug du post
+                    $slug = get_post_field('post_name', get_post()); // Récupère le slug du post
+                    if ($slug == 'stages') {
+                        $image_url = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M192 96a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm-8 384l0-128 16 0 0 128c0 17.7 14.3 32 32 32s32-14.3 32-32l0-288 56 0 64 0 16 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-16 0 0-64 192 0 0 192-192 0 0-32-64 0 0 48c0 26.5 21.5 48 48 48l224 0c26.5 0 48-21.5 48-48l0-224c0-26.5-21.5-48-48-48L368 0c-26.5 0-48 21.5-48 48l0 80-76.9 0-65.9 0c-33.7 0-64.9 17.7-82.3 46.6l-58.3 97c-9.1 15.1-4.2 34.8 10.9 43.9s34.8 4.2 43.9-10.9L120 256.9 120 480c0 17.7 14.3 32 32 32s32-14.3 32-32z"/></svg>';
+                    } elseif ($slug == 'emplois-disponibles') {
+                        $image_url = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M184 48l144 0c4.4 0 8 3.6 8 8l0 40L176 96l0-40c0-4.4 3.6-8 8-8zm-56 8l0 40L64 96C28.7 96 0 124.7 0 160l0 96 192 0 128 0 192 0 0-96c0-35.3-28.7-64-64-64l-64 0 0-40c0-30.9-25.1-56-56-56L184 0c-30.9 0-56 25.1-56 56zM512 288l-192 0 0 32c0 17.7-14.3 32-32 32l-64 0c-17.7 0-32-14.3-32-32l0-32L0 288 0 416c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-128z"/></svg>';
+                    } elseif ($slug == 'continuation-a-luniversite') {
+                        $image_url = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M219.3 .5c3.1-.6 6.3-.6 9.4 0l200 40C439.9 42.7 448 52.6 448 64s-8.1 21.3-19.3 23.5L352 102.9l0 57.1c0 70.7-57.3 128-128 128s-128-57.3-128-128l0-57.1L48 93.3l0 65.1 15.7 78.4c.9 4.7-.3 9.6-3.3 13.3s-7.6 5.9-12.4 5.9l-32 0c-4.8 0-9.3-2.1-12.4-5.9s-4.3-8.6-3.3-13.3L16 158.4l0-71.8C6.5 83.3 0 74.3 0 64C0 52.6 8.1 42.7 19.3 40.5l200-40zM111.9 327.7c10.5-3.4 21.8 .4 29.4 8.5l71 75.5c6.3 6.7 17 6.7 23.3 0l71-75.5c7.6-8.1 18.9-11.9 29.4-8.5C401 348.6 448 409.4 448 481.3c0 17-13.8 30.7-30.7 30.7L30.7 512C13.8 512 0 498.2 0 481.3c0-71.9 47-132.7 111.9-153.6z"/></svg>';
+                    } else {
+                        $image_url = '<img src="https://gftnth00.mywhc.ca/tim14/wp-content/uploads/2024/10/placeholder.png" alt="placeholder">'; // Image par défaut
+                    }
                 ?>
                     <div class="banniere" data-id="<?php the_ID(); ?>">
                         <div class="image-container">
-                            <img src="<?= "https://gftnth00.mywhc.ca/tim14/wp-content/uploads/2024/10/placeholder.png"; ?>" alt="placeholder">
+                            <?php echo $image_url; ?> <!-- Afficher le SVG ou l'image -->
                             <h2><?php the_title(); ?></h2>
                         </div>
                     </div>
@@ -36,6 +48,7 @@
                 <h1 id="cours-name"></h1>
                 <div class="text"></div>
                 <?php
+                // Réafficher le contenu du post dans un template
                 while ($query->have_posts()) : $query->the_post();
                 ?>
                     <template id="post-content-<?php the_ID(); ?>">
@@ -53,7 +66,3 @@
 
         wp_reset_postdata();
         ?>
-    </div>
-</main>
-
-<?php get_footer(); ?>
