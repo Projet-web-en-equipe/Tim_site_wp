52be6c22fbc5dbb4fc29bbaa28acb0ace32f0399
root <e2159338@cmaisonneuve.qc.ca>
2024-12-14 21:08:32 -0500

    fix erreur 404

--- 404.php
+++ 404.php
@@ -4,12 +4,12 @@
     <div id="erreur_404" class="global">
         <!-- Section pour le texte d'erreur à l'intérieur de #erreur_404 -->
         <section id="texteErreur">
-            <h2 id="titre404">Erreur 404</h2>
-            <span></span>
+            <div id="imageVoilier">
+                <img src="https://gftnth00.mywhc.ca/tim14/wp-content/uploads/2024/11/RenduVoilierCasseERIC.jpg" alt="voilierCasserERIC">
+                <h2 id="titre404">Erreur 404</h2>
+            </div>
             <p>Vous essayez d'accéder à une page qui n'existe pas et par conséquent...ÉRIC S'EST NOYÉ !!!</p>
-            <span></span>
             <p>Revenez à la page d'accueil à l'aide de l'icône du TIM</p>
-            <span></span>
         </section>
     </div>
 </main>
